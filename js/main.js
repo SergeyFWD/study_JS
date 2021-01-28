4662270cc6c2b27e733bdf167c3cd4439ad3095e
Sergey Bolshakov <sergeybo2012@yandex.ru>
2021-01-28 20:06:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let money = 123;
+let income = 300;
+let addExpenses = 100;
+let deposit = 1200;
+const period = 'month';
+
+alert('Hello World');
+console.log('Мой депозит состовляет: ' + deposit + '$');
