8ffd377ea773ef6569925855bd7b3e099c83d38c
Sergey Bolshakov <sergeybo2012@yandex.ru>
2021-01-29 21:28:39 +0300

    Сделал задание 02

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-let money = 123;
-let income = 300;
-let addExpenses = 100;
-let deposit = 1200;
-const period = 'month';
+let money = 15000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, такси, коммуналка';
+let deposit = true;
+let mission = 1000000;
+let period = 6;
 
-alert('Hello World');
-console.log('Мой депозит состовляет: ' + deposit + '$');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay = money / 30;
+console.log('Дневной бюджет: ' + budgetDay + ' рублей');
